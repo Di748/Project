0f48d5c5167804f0a8a74590b717828fc30e5ab0
Diana <golubevadiana356@gmail.com>
2021-12-14 21:10:18 +0200

    refactor. hocs

--- practice-project-pd2021-1-main/client/src/components/HOCs/OnlyNotAuthorizedUserHoc.js
+++ practice-project-pd2021-1-main/client/src/components/HOCs/OnlyNotAuthorizedUserHoc.js
@@ -2,25 +2,58 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { onlyForNotAuthorize } from '../../actions/actionCreator';
 import Spinner from '../Spinner/Spinner';
+
 const OnlyNotAuthorizedUserHoc = (Component) => {
   const mapStateToProps = (state) => state.userStore;
+
   const mapDispatchToProps = (dispatch) => ({
     checkAuth: (data) => dispatch(onlyForNotAuthorize(data)),
   });
+
+
+
+
   class HocForLoginSignUp extends React.Component {
+
     componentDidMount() {
-      this.props.checkAuth(this.props.history.replace);
+
+      const {
+
+        checkAuth,
+
+        history: { replace },
+
+      } = this.props;
+
+      checkAuth(replace);
+
     }
+
+
+
+
     render() {
-      if (this.props.isFetching) {
+
+      const { isFetching, data, history } = this.props;
+
+      if (isFetching) {
+
         return <Spinner />;
+
       }
-      if (!this.props.data) {
-        return <Component history={this.props.history} />;
+
+      if (!data) {
+
+        return <Component history={history} />;
+
       }
+
       return null;
+
     }
   }
+
   return connect(mapStateToProps, mapDispatchToProps)(HocForLoginSignUp);
 };
+
 export default OnlyNotAuthorizedUserHoc;
